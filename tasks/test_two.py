4ef0aec568efd6a30711ae8972a28739cf5a8333
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-08-27 12:48:03 -0400

    chapter 2

--- tasks/test_two.py
+++
@@ -1,4 +0,0 @@
-def test_failing():
-    print('start')
-    assert (1, 2, 3) == (3, 2, 1)
-    print('end')
--- tests/unit/test_task.py
+++ tests/unit/test_task.py
@@ -1,10 +1,5 @@
 """Test the Task data type."""
-import pytest
-from collections import namedtuple
-
-
-Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
-Task.__new__.__defaults__ = (None, None, False, None)
+from tasks import Task
 
 
 def test_asdict():
@@ -17,10 +12,25 @@ def test_asdict():
                 'id': 21}
     assert t_dict == expected
 
-@pytest.mark.run_these_please
+
 def test_replace():
     """replace() should change passed in fields."""
     t_before = Task('finish book', 'brian', False)
     t_after = t_before._replace(id=10, done=True)
     t_expected = Task('finish book', 'brian', True, 10)
     assert t_after == t_expected
+
+
+def test_defaults():
+    """Using no parameters should invoke defaults."""
+    t1 = Task()
+    t2 = Task(None, None, False, None)
+    assert t1 == t2
+
+
+def test_member_access():
+    """Check .field functionality of namedtuple."""
+    t = Task('buy milk', 'brian')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brian'
+    assert (t.done, t.id) == (False, None)
